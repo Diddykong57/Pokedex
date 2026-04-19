1e34214db7573fbe0e388f5662f2b0bcf6037dac
nicolas <nicolas.schutzing@bd-orange.com>
2026-04-19 16:57:42 +0200

    feat: implement healthCheck handler

---
+++ src/handlers/healthCheck.ts
@@ -0,0 +1,8 @@
+import type {ApiResponse} from "../global/types/api";
+import {handleRequest} from "./utils/handleRequest";
+
+export const healthCheckHandler = async (): Promise<ApiResponse> => {
+    return handleRequest(async () => {
+        return { message: "Lambda connection - Success" };
+    });
+};
