e780b3141a503ca97fca35c45cdfc58c5d98a36f
nicolas <nicolas.schutzing@bd-orange.com>
2026-04-19 18:33:45 +0200

    feat: add createPokemon (dynamoDb)

--- src/repositories/impl/dynamoDB/dynamoPokemonRepository.ts
+++ src/repositories/impl/dynamoDB/dynamoPokemonRepository.ts
@@ -1,11 +1,34 @@
 import type { Pokemon } from "../../../models/pokemon";
 import { toPokemonItems } from "../../../mappers/pokemonMapper";
 import type { PokemonRepository } from "../../pokemonRepository";
+import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+    UpdateCommand,
+    PutCommand,
+    DynamoDBDocumentClient,
+    ScanCommand,
+    DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
 
 export class DynamoPokemonRepository implements PokemonRepository {
     async create(pokemon: Pokemon): Promise<void> {
         const [metadataItem, statsItem] = toPokemonItems(pokemon);
 
-        // Add in DynamoDB
+        await docClient.send(
+            new PutCommand({
+                TableName: process.env.TABLE_NAME,
+                Item: metadataItem,
+            })
+        );
+
+        await docClient.send(
+            new PutCommand({
+                TableName: process.env.TABLE_NAME,
+                Item: statsItem,
+            })
+        );
     }
 }
