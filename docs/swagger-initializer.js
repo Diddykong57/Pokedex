bc03687c0092c8e83fc52637f961ad0ab75782ab
nicolas <nicolas.schutzing@bd-orange.com>
2026-04-20 03:41:20 +0200

    fix: use swagger with Oauth

---
+++ docs/swagger-initializer.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    const redirectUrl = window.location.origin + "/docs/oauth2-redirect.html";
+
+    const ui = SwaggerUIBundle({
+        url: "./openapi.yaml",
+        dom_id: "#swagger-ui",
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout",
+        oauth2RedirectUrl: redirectUrl
+    });
+
+    ui.initOAuth({
+        clientId: "5e1sv0erm0h9nhhtvin5lh7cci",
+        usePkceWithAuthorizationCodeGrant: true,
+        scopes: "openid email"
+    });
+
+    window.ui = ui;
+};
