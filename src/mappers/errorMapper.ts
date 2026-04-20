b539ae4da552fbd593b77c5a775dc0635cd62bf1
nicolas <nicolas.schutzing@bd-orange.com>
2026-04-20 03:48:01 +0200

    fix: add CORS in error callback

--- src/mappers/errorMapper.ts
+++ src/mappers/errorMapper.ts
@@ -4,6 +4,12 @@ import { HTTP } from "../global/constants/httpStatus";
 import { ERROR_MESSAGES } from "../global/constants/errorMessages";
 
 export function toApiErrorResponse(error: unknown): ApiResponse {
+    const corsHeaders = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Requested-With",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+        "Content-Type": "application/json"
+    }
     if (isHttpError(error)) {
         // Get error.details.details if present, otherwise undefined
         const details = (error.details as { details?: string[] } | undefined)?.details;
@@ -11,6 +17,7 @@ export function toApiErrorResponse(error: unknown): ApiResponse {
         if (details) {
             return {
                 statusCode: error.statusCode,
+                headers: corsHeaders,
                 body: JSON.stringify({
                     message: error.message,
                     details: details,
@@ -20,6 +27,7 @@ export function toApiErrorResponse(error: unknown): ApiResponse {
 
         return {
             statusCode: error.statusCode,
+            headers: corsHeaders,
             body: JSON.stringify({
                 message: error.message,
             }),
@@ -28,6 +36,7 @@ export function toApiErrorResponse(error: unknown): ApiResponse {
 
     return {
         statusCode: HTTP.INTERNAL_SERVER_ERROR,
+        headers: corsHeaders,
         body: JSON.stringify({
             message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR,
         }),
