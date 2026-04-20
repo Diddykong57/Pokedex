66d96e48260efb50b8b128f3ab2cadcd46f542bc
nicolas <nicolas.schutzing@bd-orange.com>
2026-04-20 04:53:53 +0200

    fix: update tests in local

--- src/local.ts
+++ src/local.ts
@@ -1,4 +1,4 @@
-import { getFakeDb } from "./repositories/impl/local/localPokemonRepository";
+import {getFakeDb, LocalPokemonRepository} from "./repositories/impl/local/localPokemonRepository";
 import { createPokemonHandler } from "./handlers/pokemon/createPokemon";
 import { getPokemonListHandler } from "./handlers/pokemon/getPokemonList";
 import { getPokemonDetailsHandler } from "./handlers/pokemon/getPokemonDetails";
@@ -6,15 +6,42 @@ import { updatePokemonHandler } from "./handlers/pokemon/updatePokemon";
 import { pokemonCreateFixture, pokemonUpdateFixture } from "./tests/fixtures/pokemon";
 import type { ApiResponse } from "./global/types/api";
 import {deletePokemonHandler} from "./handlers/pokemon/deletePokemon";
+import type {PokemonRepository} from "./repositories/pokemonRepository";
+import {DynamoPokemonRepository} from "./repositories/impl/dynamoDB/dynamoPokemonRepository";
+import type {PokemonService} from "./services/pokemonService";
+import {PokemonServiceImpl} from "./services/impl/pokemonServiceImpl";
+
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Requested-With",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+        "Content-Type": "application/json"
+}
+
+
+
+const repository: PokemonRepository =
+    process.env.APP_ENV === "aws"
+        ? new DynamoPokemonRepository()
+        : new LocalPokemonRepository();
+const service: PokemonService = new PokemonServiceImpl(repository);
 
 async function main() {
     let response: ApiResponse;
-    await createPokemonHandler({ body: JSON.stringify(pokemonCreateFixture[0]) });
-    await createPokemonHandler({ body: JSON.stringify(pokemonCreateFixture[1]) });
-    await createPokemonHandler({ body: JSON.stringify(pokemonCreateFixture[2]) });
-    await createPokemonHandler({ body: JSON.stringify(pokemonCreateFixture[3]) });
+    response = await createPokemonHandler(service, { body: JSON.stringify(pokemonCreateFixture[0]) });
 
-    response = await getPokemonListHandler();
+    console.log("[getPokemonListHandler[0]] HTTP response:");
+    console.log(response);
+
+    await createPokemonHandler(service, { body: JSON.stringify(pokemonCreateFixture[1]) });
+    await createPokemonHandler(service, { body: JSON.stringify(pokemonCreateFixture[2]) });
+    await createPokemonHandler(service, { body: JSON.stringify(pokemonCreateFixture[3]) });
+    response = await createPokemonHandler(service, { body: JSON.stringify(pokemonCreateFixture[0]) });
+
+    console.log("[getPokemonListHandler[4]] HTTP response:");
+    console.log(response);
+
+    response = await getPokemonListHandler(service);
 
     console.log("[getPokemonListHandler] HTTP response:");
     console.log(response);
@@ -22,12 +49,12 @@ async function main() {
     const parsedBody = JSON.parse(response.body);
     const id = parsedBody[0].id;
 
-    response = await getPokemonDetailsHandler({pathParameters: {id}});
+    response = await getPokemonDetailsHandler(service, {pathParameters: {id}});
 
     console.log("[getPokemonDetailsHandler] HTTP response:");
     console.log(response);
 
-    response = await updatePokemonHandler({
+    response = await updatePokemonHandler(service, {
         pathParameters: {
             id,
         },
@@ -37,12 +64,12 @@ async function main() {
     console.log("[updatePokemonHandler] HTTP response:");
     console.log(response);
 
-    response = await deletePokemonHandler({pathParameters: {id}});
+    response = await deletePokemonHandler(service, {pathParameters: {id}});
 
     console.log("[deletePokemonHandler] HTTP response:");
     console.log(response);
 
-    response = await getPokemonListHandler();
+    response = await getPokemonListHandler(service);
 
     console.log("[getPokemonListHandler] HTTP response:");
     console.log(response);
